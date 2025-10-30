10c46e9f69ca1f55a7bb491dde6ee012876e2791
kostya noskov <kostay_noskov_97@mail.ru>
2025-10-30 18:55:16 +0500

    urls

--- gagproject/hello/urls.py
+++ gagproject/hello/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import hello
+from .views import page1, page2, page3, page4, page5
 
 urlpatterns = [
-        path('', hello),
+        path('', page1),
+        path('2', page2),
+        path('3', page3),
+        path('4', page4),
+        path('5', page5),
 ]
--- gagproject/hello/views.py
+++ gagproject/hello/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render, HttpResponse
 from .models import streams
 
 # Create your views here.
-def hello(request):
+def page1(request):
     name = streams.objects.all()
     return render(request, 'base.html', {'name': name})
 
+def page2(request):
+
+    return render(request, 'page-2.html')
+
+def page3(request):
+
+    return render(request, 'page-3.html')
+    
+def page4(request):
+
+    return render(request, 'page-4.html')
+
+def page5(request):
+
+    return render(request, 'page-5.html')
